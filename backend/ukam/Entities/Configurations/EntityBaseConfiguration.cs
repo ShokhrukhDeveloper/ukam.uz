0848cf3967960de6aebe93bb336eac5ddd0ab8d7
1brokxm <98509909+1brokxm@users.noreply.github.com>
2022-10-03 15:18:24 +0500

    updated

---
+++ backend/ukam/Entities/Configurations/EntityBaseConfiguration.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backend.Uckam.Entities.Configurations;
+
+public class EntityBaseConfiguration<T> : IEntityTypeConfiguration<T> where T : EntityBase
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.Id)
+                .HasColumnType("ulong")
+                .ValueGeneratedOnAdd();
+        builder.Property(b => b.CreatedAt).IsRequired();
+        builder.Property(b => b.UpdatedAt).IsRequired();
+    }
+
+}
---
+++ backend/ukam/Program.cs
@@ -0,0 +1,28 @@
+using Backend.Uckam.data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options
+        .UseSqlite(builder.Configuration
+        .GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
